868133d2b615d2a2323a0e3cb0eeb600abf16563
Michel Milmore <m.milmore2701@gmail.com>
2022-09-01 20:52:06 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -3,21 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <form action="">
-          <h2>TODO</h2>
-          <label htmlFor="">What needs to be done?</label>
-          <br />
-          <input type="text" />
-          <button>Add #!</button>
-          <ul>
-            <li>Item 1</li>
-            <li>Item 2</li>
-            <li>Item 3</li>
-            <li>Item 4</li>
-          </ul>
-        </form>
-      </header>
+      <header className="App-header">NDCC 2022 Formulaire P/A 1-D</header>
     </div>
   );
 }
